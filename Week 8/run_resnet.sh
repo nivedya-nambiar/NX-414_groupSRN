d892ac3d6a2a6fcc5f1b6712482d10198ae4f910
roalmeid <rudras2003@gmail.com>
2025-05-06 19:36:30 +0200

    run resnet mine

---
+++ Week 8/run_resnet.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=ridge_all_layers_tuning          # job name
+#SBATCH --output=outputofpyfile.log
+
+
+#SBATCH --gres=gpu:1              # request 1 GPU, commented out
+#SBATCH --mem=64G                 # memory
+#SBATCH --cpus-per-task=4         # optional: useful for dataloading
+#SBATCH --time=01:00:00           # 2-hour time limit
+
+# Load your environment/modules
+# module load python/3.9            # only if your system uses modules
+
+# Activate your virtual environment (adjust to your setup)
+source conda activate nx414N  # or conda activate myenv
+
+# Run your Python script
+#python test.py
+python test2.py
